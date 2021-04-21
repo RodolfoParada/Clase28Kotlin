ed6e64348d31c810759aaf0dfaab29b9b2c697dc
Vuelamarte123 <rodolfo.parada.gonzalez@gmail.com>
2021-04-20 23:09:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/clase28kotlin/APIService.kt
@@ -0,0 +1,11 @@
+package com.example.clase28kotlin
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIService {
+    @GET
+    fun getCharacterByName(@Url url:String): Call<DogsResponse>
+}
+
---
+++ app/src/main/java/com/example/clase28kotlin/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.clase28kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl("https://dog.ceo/api/breed/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
+    private fun searchByName(query: String) {
+        doAsync {
+            val call = getRetrofit().create(APIService::class.java).getCharacterByName("$query/images").execute()
+            val puppies = call.body() as DogsResponse
+            uiThread {
+                if(puppies.status == "success") {
+                    initCharacter(puppies)
+                }else{
+                    showErrorDialog()
+                }
+                hideKeyboard()
+            }
+        }
+    }
+   }
